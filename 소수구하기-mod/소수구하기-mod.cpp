5764cb9885845567003d94c21592f3ac7836de39
lilly <lilly1987@naver.com>
2018-09-01 20:25:54 +0900

    goto 제거중 에라 몰르겠다 여기까지 할련다

--- 소수구하기-mod/소수구하기-mod.cpp
+++ 소수구하기-mod/소수구하기-mod.cpp
@@ -17,46 +17,36 @@ int main()
 	// 만일 n이 1보다 작거나 같으면 함수 종료
 	if (n <= 1) return 0;
 	  
-goto1:
-	
-	q = 2;
-	s = 0;
+	do {
+		q = 1;
+		s = 0;
+		
+		do {
+			q += 1;
+			if (q < p) {
+				m = p % q;
+			}
+			else {
+				goto goto3;
+			}
 
-goto2:
-	
-	if (q<p) {
-		m = p % q;
-	}
-	else {
-		goto goto3;
-	}
-	if (m == 0) {
-	
-	}
-	else {
-		q += 1;
-		goto goto2;
-	}
-	s = 1;
+		} while (m != 0);
+		
+		s = 1;
 
-goto3:
+	goto3:
 
-	if (s == 0) {
-		cout <<p<<",";
-		c += 1;
-		if (c%5==0)
-		{
-			cout << endl;
+		if (s == 0) {
+			cout << p << ",";
+			c += 1;
+			if (c % 5 == 0)
+			{
+				cout << endl;
+			}
 		}
-	}
-	else {
+		p += 1;
+	} while (p < n);
 
-	}
-	p += 1;
-	if (p<n)
-	{
-		goto goto1;
-	}
 	cout << c << endl;;
 	return 0;
 }
